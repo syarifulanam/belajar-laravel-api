7c87b55190fffb31617e247a731c3fe885571cce
Anam <anam@anams-MacBook-Pro.local>
2025-05-22 10:58:33 +0700

    membuat API di laravel dengan studi kasus To Do List

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,12 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    public function index()
+    {
+        $todos = Todo::all();
+        
+        return response()->json(['data' => $todos]);
+    }
+
+    public function show($id)
+    {
+        $todo = Todo::findOrFail($id);
+
+        return response()->json(['data' => $todo]);
+    }    
+
     public function store(Request $request)
     {
-        return 'hello ini adalah function store untuk todo list';
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $todo = Todo::create($request->all());
+        $todo->refresh();
+
+        return response()->json(['data' => $todo]);
+    }
+
+    public function update($id, Request $request)
+    {
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        $todo = Todo::findOrFail($id);
+        $todo->name = $request->name;
+        $todo->save();
+
+        return response()->json(['data' => $todo]);
+    }
+
+    public function destroy($id)
+    {
+        $todo = Todo::findOrFail($id);
+
+        $todo->delete();
+
+        return response()->json(['data' => $todo]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 Route::post('/todos', [TodoController::class, 'store']);
+Route::patch('/todos/{id}', [TodoController::class, 'update']);
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
+Route::get('/todos', [TodoController::class, 'index']);
+Route::get('/todos/{id}', [TodoController::class, 'show']);
